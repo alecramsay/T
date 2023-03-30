9fc6c2aeaa77ec8c3a2db2295cbf62d60ab43218
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-03-30 07:17:47 -0700

    Fleshed out 'groupby' agg fns

--- t/datamodel.py
+++ t/datamodel.py
@@ -41,8 +41,7 @@ PD_DESCRIBE_FNS: list[str] = ["count", "mean", "std", "min", "25%", "50%", "75%"
 PD_DESCRIBE_TYPES: list[str] = ["int64", "float64", "datetime64", "timedelta64"]
 
 # Pandas agg functions for groupby()
-PD_AGG_FNS: list[str] = ["count", "mean", "std", "min", "max"]
-# AGG_FNS: list[str] = ["count", "min", "max", "std", "sum", "mean", "median"]
+PD_AGG_FNS: list[str] = ["sum", "count", "mean", "std", "min", "max", "median"]
 
 
 class Column:
--- test/lang/groupby2.t
+++ test/lang/groupby2.t
@@ -1,6 +1,3 @@
 from('2020_census_AZ(PARTIAL).csv')
 derive(county_fips, GEOID20[2:5])
-# keep(GEOID20, county_fips, Tot_2020_tot)
-# rename((Tot_2020_tot, Total))
-groupby(by=[county_fips])
-# groupby(by=[county_fips], only=[Tot_2020_tot], agg=[sum]) # TODO: this should work
+groupby(by=[county_fips], only=[Tot_2020_tot], agg=[sum, min, max, mean, median])
